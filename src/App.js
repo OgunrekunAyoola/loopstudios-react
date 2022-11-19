d95370ce0b0eac5484306f7ea27de1d62cc4c7da
Naetan07 <oreofeogunrekun@gmail.com>
2022-11-19 08:47:36 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Header from "./components/header";
+import Header from "./components/header"
 import Showcase from "../src/components/showcase"
-import Interactive from "./components/interactive";
-import Creations from "./components/creation";
+import Interactive from "./components/interactive"
+import Creations from "./components/creation"
 import Css from "../src/App.css"
 
 function App(){
--- src/components/creation.jsx
+++ src/components/creation.jsx
@@ -1,30 +1,52 @@
-import React, { useState } from 'react'
-import data from "../data-mobile"
  
 
+import React from 'react'
+import Deep__earth from  "../assets/Mobile/image-deep-earth.jpg"
+import Night__arcade from "../assets/Mobile/image-night-arcade.jpg"
+import Soccer__team from "../assets/Mobile/image-soccer-team.jpg"
+import The__grid from "../assets/Mobile/image-grid.jpg"
+import Above__vr from "../assets/Mobile/image-from-above.jpg"
+import Pocket__borealis from "../assets/Mobile/image-pocket-borealis.jpg"
+import The__curiosity from "../assets/Mobile/image-curiosity.jpg"
+import Make__it__fisheye from "../assets/Mobile/image-fisheye.jpg"
+
+
 const Creations = () => {
-     const[cards, setCards] = useState(data)
 
      return (
-        <div>
-            <h2>Our creations</h2>
-        
-          <section>
-            {cards.map((Cards) => {
-                const{id, image, title} = card
-                return(
-                    <article key={ id }>
-                        <div>
-                            <img src={ image } alt="creationcard" />
-                        </div>
-                        <h3>{ title }</h3> 
-                    </article> 
-                )
-            })}
-          </section>
-          <div className='btn-container'>
-            <button className='btn'>SEE ALL</button>
-           </div>
+        <div className="mobile__creation">
+            <div>
+                <img src={ Deep__earth } alt="" />
+                <h3>DEEP EARTH</h3>
+            </div>
+            <div>
+                <img src={ Night__arcade } alt="" />
+                <h3>NIGHT ARCADE</h3>
+            </div>
+            <div>
+                <img src={ Soccer__team } alt="" />
+                <h3>SOCCER TEAM</h3>
+            </div>
+            <div>
+                <img src={The__grid } alt="" />
+                <h3>THE GRID</h3>
+            </div>
+            <div>
+                <img src={ Above__vr } alt="" />
+                <h3>ABOVE VR</h3>
+            </div>
+            <div>
+                <img src={ Pocket__borealis } alt="" />
+                <h3>POCKET BOREALIS</h3>
+            </div>
+            <div>
+                <img src={ The__curiosity } alt="" />
+                <h3>THE CURIOSITY</h3>
+            </div>
+            <div>
+                <img src={ Make__it__fisheye } alt="" />
+                <h3>MAKE IT FISHEYE</h3>
+            </div>
         </div>
      )
 }
